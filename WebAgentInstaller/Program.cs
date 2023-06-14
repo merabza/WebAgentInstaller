b70b58ec33496dc3da2617a6301bb1d04ca6cc93
merabza <merabza@gmail.com>
2023-06-14 18:18:30 +0400

    Initial

---
+++ WebAgentInstaller/Program.cs
@@ -0,0 +1,61 @@
+using System;
+using System.IO;
+using FluentValidationInstaller;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using SystemToolsShared;
+using WebInstallers;
+using AssemblyReference = TestToolsMini.AssemblyReference;
+
+//პროგრამის ატრიბუტების დაყენება 
+ProgramAttributes.Instance.SetAttribute("AppName", "WebAgentInstaller");
+ProgramAttributes.Instance.SetAttribute("VersionCount", 1);
+ProgramAttributes.Instance.SetAttribute("UseSwaggerWithJWTBearer", false);
+ProgramAttributes.Instance.SetAttribute("AppKey", "C0090D12-DC4F-4A15-A7F8-A024E241DB1A");
+
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    ContentRootPath = AppContext.BaseDirectory,
+    Args = args
+});
+
+builder.InstallServices(args,
+    AssemblyReference.Assembly,
+    LibProjectsMini.AssemblyReference.Assembly,
+    ConfigurationEncrypt.AssemblyReference.Assembly,
+    SerilogLogger.AssemblyReference.Assembly,
+    WindowsServiceTools.AssemblyReference.Assembly,
+    SwaggerTools.AssemblyReference.Assembly,
+    ApiExceptionHandler.AssemblyReference.Assembly
+);
+
+builder.Services.AddMediatR(cfg => { cfg.RegisterServicesFromAssembly(LibProjectsMini.AssemblyReference.Assembly); });
+
+// FluentValidationInstaller
+builder.Services.InstallValidation(LibProjectsMini.AssemblyReference.Assembly);
+
+var app = builder.Build();
+
+app.UseServices();
+
+
+try
+{
+    Log.Information("Directory.GetCurrentDirectory() = {0}", Directory.GetCurrentDirectory());
+    Log.Information("AppContext.BaseDirectory = {0}", AppContext.BaseDirectory);
+
+    app.Run();
+
+    Log.Information("Finish");
+    return 0;
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "Host terminated unexpectedly");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
